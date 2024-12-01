159bef8376d7f8c770a1a019aeeaa5ba252456ed
Gadielo03 <gadielo2303@gmail.com>
2024-11-30 19:09:52 -0600

    update redirect URI for local development in auth.jsx

--- src/auth.jsx
+++ src/auth.jsx
@@ -3,11 +3,10 @@ const CLIENT_ID = "0ab6b75d95f141509426ad5345280550"
 //const REDIRECT_URI = "https://gadielo03.github.io/"
 //const REDIRECT_URI = "https://my.home-assistant.io/redirect/supervisor_ingress/?addon=https%3A%2F%2Fgadielo03.github.io%2Fspotify-widget%2F"
 //const REDIRECT_URI = "https://gadielo03.github.io/spotify-widget/callback"
-const REDIRECT_URI = "http://localhost:8888/callback"
+const REDIRECT_URI = "http://localhost:5173/"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
 
 const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-currently-playing user-read-playback-state`
 
 export { loginUrl }
-
